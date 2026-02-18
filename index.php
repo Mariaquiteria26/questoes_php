d27dc5c9c0f0829c22b88ef948a365b8ee020f65
Mariah <mariahquiteriaa@gmail.com>
2026-02-18 12:06:10 -0300

    Add files via upload
    evdgg

--- index.php
+++ index.php
@@ -3,23 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Questão 1</title>
+    <title>Document</title>
 </head>
 <body>
-    <form method="post" action="">
-        <label>Número 1:</label>
-        <input type="number" name="num1" required><br><br>
-        <label>Número 2:</label>
-        <input type="number" name="num2" required><br><br>
-        <input type="submit" name="somar" value="Somar">
-    </form>
-    <?php
-        
-        $numero1 = $_POST['num1'];
-        $numero2 = $_POST['num2'];
-        $soma = $numero1 + $numero2;
-        echo "<h3>O resultado da soma é: $soma</h3>";
-    ?>
+    
 </body>
 </html>
-
