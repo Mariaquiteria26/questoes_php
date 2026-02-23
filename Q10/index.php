a2cae35d53ce81c4bb572b018279ce18105dc258
Mariah <mariahquiteriaa@gmail.com>
2026-02-23 19:52:02 -0300

    Add files via upload
    mais duas questoes

--- Q10/index.php
+++ Q10/index.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Loja mamão com açucar</title>
 </head>
 <body>
-    
+    <form method="get" action="">
+        <label>Valor do produto:</label>
+        <input type="number" name="valor" step="0.01" required><br><br>
+        <input type="submit" name="calcular" value="Calcular">
+    </form>
+
+    <?php
+    if (isset($_GET['valor'])) {
+        $valor = $_GET["valor"];
+        $prestacao = $valor / 5;
+
+        echo "<h3>Resultado:<h3>";
+        echo "<h3>Valor do produto: R$ $valor</h3>";
+        echo "<h3>Valor da prestação: R$ $prestacao</h3>";
+    }
+    ?>
+       
 </body>
 </html>
---
+++ Q11/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculo de valor de venda</title>
+</head>
+<body>
+    <form method="get" action="">
+        <label>Preço de custo:</label>
+        <input type="number" name="custo" step="0.01" required><br><br>
+        <label>percentual de acrescimo:</label>
+        <input type="number" name="acrescimo" step="0.01" required><br><br>
+        <input type="submit" name="calcular" value="Calcular">
+    </form>
+
+    <?php
+    if (isset($_GET['calcular'])) {
+        $custo = $_GET['custo'];
+        $percentual = $_GET['acrescimo'];
+        $acrescimo = $custo * ($percentual / 100);
+        $valor_venda = $custo + $acrescimo;
+
+        echo "<h3>Resultado:</h3>";
+        echo "<h3>Preço de custo: R$ $custo</h3>";
+        echo "<h3>Percentual de acrescimo: $percentual%</h3>";
+        echo "<h3>O valor de venda é: R$ $valor_venda</h3>";
+    }
+    ?>
+</html>
